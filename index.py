d3bdf6e8eaa54277fb5eeea1968557740ee2024b
Jason Boyle <jb5531@gmail.com>
2023-10-31 15:09:53 -0400

    Move ball into a class
    Thinking about moving paddles / player and opponent into a class too...

--- index.py
+++ index.py
@@ -1,37 +1,77 @@
 import pygame, sys, random
 
-def ball_animation():
-    global ball_speed_x, ball_speed_y, player_score, opponent_score
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+class Ball():
+    def __init__(self, player, opponent):
+    # def __init__(self, player):
+        # super().__init__()
+        self.speed_x = 7 * random.choice((1,-1))
+        self.speed_y = 7 * random.choice((1,-1))
+        self.rect = pygame.Rect(screen_width/2 -15, screen_height/2 - 15, 30, 30)
+        self.player = player
+        self.opponent = opponent
 
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        if ball.left <= 0: player_score += 1
-        else: opponent_score += 1
-        ball_restart()
+    def draw(self):
+        pygame.draw.ellipse(screen, light_grey, self.rect)
+    
+    def animation_state(self):
+        global player_score, opponent_score
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
 
-def opponent_ai():
-    if opponent.top < ball.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:
-        opponent.bottom -= opponent_speed
+        if self.rect.top <= 0 or self.rect.bottom >= screen_height:
+            self.speed_y *= -1
 
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= screen_height:
-        opponent.bottom = screen_height
+        if self.rect.left <= 0 or self.rect.right >= screen_width:
+            if self.rect.left <= 0: player_score += 1
+            else: opponent_score += 1
+            self.restart()
 
-def ball_restart():
-    global ball_speed_y, ball_speed_x, game_active
-    game_active = False
-    ball.center = (screen_width/2, screen_height/2)
-    ball_speed_y *= random.choice((1,-1))
-    ball_speed_x *= random.choice((1,-1))
+        if self.rect.colliderect(self.player) or self.rect.colliderect(self.opponent):
+            self.speed_x *= -1
+
+        self.draw()
+    
+    def restart(self):
+        global game_active
+        game_active = False
+
+        self.rect.center = (screen_width/2, screen_height/2)
+        self.speed_x = 7 * random.choice((1,-1))
+        self.speed_y = 7 * random.choice((1,-1))
+
+    def opponent_ai(self):
+        global opponent_speed
+
+        if self.opponent.top < self.rect.y:
+            self.opponent.top += opponent_speed
+        if self.opponent.bottom > self.rect.y:
+            self.opponent.bottom -= opponent_speed
+
+        if self.opponent.top <= 0:
+            self.opponent.top = 0
+        if self.opponent.bottom >= screen_height:
+            self.opponent.bottom = screen_height
+
+    def update(self):
+        self.animation_state()
+        self.opponent_ai()
+
+# class Paddle():
+#     def __init__(self, left, top, color):
+#         self.color = color
+#         self.left = left
+#         self.top = top
+#         # TODO: Do I need to set self.left and self.top?
+#         self.rect = pygame.Rect(left, top, 10, 140)
+
+#     def draw(self):
+#         pygame.draw.rect(screen, light_grey, self.rect)
+
+# class Opponent(Paddle):
+#     def __init__(self, left, top, color):
+#         super().__init__(left, top, color)
+#         self.speed = 7
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -49,13 +89,16 @@ game_active = False
 player_score = 0
 opponent_score = 0
 
-# Game rectangles
-ball = pygame.Rect(screen_width/2 -15, screen_height/2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
-
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
+
+# Game rectangles
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+# opponent = Opponent(10, screen_height/2 - 70, light_grey)
+# print(opponent)
+
+
 
 game_name = large.render('Pong', False, light_grey)
 game_name_rect = game_name.get_rect(center = (640,200))
@@ -63,8 +106,7 @@ game_name_rect = game_name.get_rect(center = (640,200))
 game_start = small.render('Press Space to Start', False, light_grey)
 game_start_rect = game_start.get_rect(center = (640,400))
 
-ball_speed_x = 7 * random.choice((1,-1))
-ball_speed_y = 7 * random.choice((1,-1))
+ball = Ball(player, opponent)
 player_speed = 0
 opponent_speed = 7
 
@@ -88,13 +130,11 @@ while True:
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
-    pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
 
     if game_active:
-        ball_animation()
-        opponent_ai()
-
+        ball.update()
+      
     else:
         screen.blit(game_name, game_name_rect)
         screen.blit(game_start, game_start_rect)
@@ -106,6 +146,8 @@ while True:
         your_score_rect = your_score.get_rect(center = (960,200))
 
         player_speed = 0
+        ball.draw()
+        # opponent.draw()
 
         screen.blit(ai_score, ai_score_rect)
         screen.blit(your_score, your_score_rect)
